986c766aeb8c9c0cc1b87117afe87172a119b225
Hesham Hany <73742274+liolah@users.noreply.github.com>
2021-06-05 23:11:54 +0200

    Temperature and breathing modes fixed

--- Project_Sky_Light/Project_Sky_Light.ino
+++ Project_Sky_Light/Project_Sky_Light.ino
@@ -1,20 +1,21 @@
 #include <BlynkSimpleEsp8266.h>
 
-#define tpin A0
-#define lpin D1
-#define ppin D2
+#define tpin A0   //temprature sensor pin
+#define lpin D1   //light sensor pin
+#define ppin D2   //pir sensor pin
 #define default_lighting_mode 1
-#define fadeValue 30   // determines the fading animation smoothness along with the timer used with light()
+#define fadeValue 10   // determines the fading animation smoothness along with the timer used with light()
 
 int pin[3] = {D5,D6,D7};
 int rgbWave[6] = {0,-512,-1023,1,1,1};   // From [0] to [2] saves the previous value. From [3] to [5] saves the wave state (rising or falling)
 int rgbSelection[3];
-int Breathing[5] = {0,0,0,1,0};
+int Breathing[5] = {0,0,0,0,0};
 float temp[2];
 //int rgb[3];
 int lightingMode = default_lighting_mode;
 bool ambientLight = true;
 bool motion = false;
+bool pmotion = false;
 bool on = true;
 bool pirArmed = true;
 
@@ -24,19 +25,19 @@ char auth[] = "qmsU01AN1dBjzbzC5oi8y0DrFNwNaQ7j";   //Auth Token : qmsU01AN1dBjz
 
 SimpleTimer timer;
 
-float naturalNum(float x){
+float naturalNum(float x){    //returns positive numbers only, if the number is negative returns 0
   if(x >= 0)
     return x;
     return 0;  
   }
 
-float limit(float x){ 
+float limit(float x){    //returns 1023 if the number is greater than 1023
   if(x <= 1023)
     return x;
-    return 0;  
+    return 1023;  
   }
 
-void breathing(){
+void breathing(){   //breathing effect function
   if(Breathing[4] == 0){
     for(int i= 0;i<3;i++){
       Breathing[i] = rgbSelection[i];
@@ -60,7 +61,7 @@ void breathing(){
   }}} 
   }
 
-void wave(){
+void wave(){    //RGB wave function
   for(int i=0;i<3;i++){
   if (rgbWave[3+i]){
    if (rgbWave[i] < 1023){
@@ -79,13 +80,13 @@ void wave(){
   }}}
   }
 
-void motionNotification(){
+void motionNotification(){    //sends a notification to the app if motion detected
  if(pirArmed){
   if(motion){
   Blynk.notify("Motion detected in the vicinity!"); }
   }}
 
-void staticLight(){
+void staticLight(){   //static light mode function
    analogWrite(pin[0],rgbSelection[0]);
    analogWrite(pin[1],rgbSelection[1]);
    analogWrite(pin[2],rgbSelection[2]); 
@@ -94,11 +95,14 @@ void staticLight(){
 //      }
   }
 
-void tempLight(){             
+void tempLight(){             //temprature dependent light mode function
   int r,g,b;
   r = limit(((naturalNum(temp[1]-15))/10)*1023);
+  Serial.println(r);
   g = min(limit(((naturalNum(temp[1]-5))/10)*1023), limit(((naturalNum(45-temp[1]))/10)*1023));
+  Serial.println(g);
   b = limit(((naturalNum(35-temp[1]))/10)*1023);
+  Serial.println(b);
    analogWrite(pin[0],r);
    analogWrite(pin[1],g);
    analogWrite(pin[2],b); 
@@ -112,11 +116,11 @@ void tempLight(){
 //    analogWrite(pin[0],rgb[i]);
 //  }}
 
-BLYNK_CONNECTED() {
+BLYNK_CONNECTED() {   //syncs the nodeMCU with the app on startup
     Blynk.syncAll();
 }
 
-BLYNK_WRITE(V4) {
+BLYNK_WRITE(V4) {  //light modes list 
   switch (param.asInt())
   {
     case 1: // Static light
@@ -140,7 +144,7 @@ BLYNK_WRITE(V4) {
   }
 }
 
-BLYNK_WRITE(V1){
+BLYNK_WRITE(V1){ // Zebra rgb 
     rgbSelection[0] = param[0].asInt();
     rgbSelection[1] = param[1].asInt();
     rgbSelection[2] = param[2].asInt();
@@ -152,11 +156,11 @@ BLYNK_WRITE(V1){
 //      Breathing[4] = 0;
 }
 
-BLYNK_WRITE(V2){   
+BLYNK_WRITE(V2){   //On switch
   on = param.asInt();
 }
 
-BLYNK_WRITE(V3){   
+BLYNK_WRITE(V3){   //Armed switch
   pirArmed = param.asInt();
 }
 
@@ -172,7 +176,8 @@ void lightsOut(){
   }
 
 void motionCheck(){
-  motion = (digitalRead(ppin)==1); 
+  motion = digitalRead(ppin); 
+  
     }
 
 void motionStill(){
